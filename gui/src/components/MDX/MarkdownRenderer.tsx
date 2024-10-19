461e81a79582a136c145f6697ef61f28a1b745ca
jianglang <lang.jiang@ximalaya.com>
2024-10-19 23:20:35 +0800

    feat: 代码块支持copy

--- gui/src/components/MDX/MarkdownRenderer.tsx
+++ gui/src/components/MDX/MarkdownRenderer.tsx
@@ -4,13 +4,39 @@ import remarkFrontmatter from "remark-frontmatter";
 import remarkDirective from "remark-directive";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
-import rehypePrism from "rehype-prism-plus";
 import { defaultComponents } from "./DefaultComponents";
 import "prism-themes/themes/prism-xonokai.min.css";
 import "./mdx.scss";
 import "./theme/tailwind-blue.dark.css";
 import { RangeInFileWithContents } from "core";
-import CodeView from "./CodeView";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+
+const CodeBlock = ({
+  language,
+  value,
+}: {
+  language: string;
+  value: string;
+}) => {
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(value);
+  };
+
+  return (
+    <div style={{ position: "relative" }}>
+      <button
+        onClick={copyToClipboard}
+        style={{ position: "absolute", right: 0, zIndex: 1 }}
+      >
+        复制
+      </button>
+      <SyntaxHighlighter language={language} style={oneDark}>
+        {value}
+      </SyntaxHighlighter>
+    </div>
+  );
+};
 
 const MarkdownRenderer = (
   props: Options & {
@@ -20,7 +46,7 @@ const MarkdownRenderer = (
   return (
     <ReactMarkdown
       className="markdown-body mb-4 overflow-hidden"
-      rehypePlugins={[rehypeKatex, rehypePrism]}
+      rehypePlugins={[rehypeKatex]}
       remarkPlugins={[
         remarkGfm,
         remarkFrontmatter,
@@ -29,7 +55,20 @@ const MarkdownRenderer = (
       ]}
       components={{
         ...defaultComponents,
-        pre: (...p) => <CodeView rangeInfo={props.rangeInfo} {...p[0]} />,
+        code(props) {
+          const { children, className, ...rest } = props;
+          const match = /language-(\w+)/.exec(className || "");
+          return match ? (
+            <CodeBlock
+              language={match[1]}
+              value={String(children).replace(/\n$/, "")}
+            />
+          ) : (
+            <code {...rest} className={className}>
+              {children}
+            </code>
+          );
+        },
       }}
       {...props}
     ></ReactMarkdown>
